72af9c2b2052e68540a876c28d1ceba5c1e1ae57
emersonsrosa <72410299+emersonsrosa@users.noreply.github.com>
2020-10-20 20:12:36 -0400

    Fiz o panda funcionar.
    o problema é que ele não funciona na versao python 3.9, só na 3.8

--- Desafio04.py
+++ Desafio04.py
@@ -1,9 +1,11 @@
-# Proporção (Porcentagem) dos por idade.
 import pandas as pd
-
 
 #fonte = 'https://github.com/alura-cursos/imersao-dados-2-2020/blob/master/MICRODADOS_ENEM_2019_SAMPLE_43278.csv?raw=true'
 fonte = './MICRODADOS_ENEM_2019_SAMPLE_43278.csv'
 
 dados = pd.read_csv(fonte)
-print(dados.columns.values)
+
+#print(dados.head())
+#print(dados.columns.values)
+
+print(dados.query('IN_TREINEIRO == 0')['NU_IDADE'].hist())
--- estanciandoPandas.py
+++ estanciandoPandas.py
@@ -1,9 +1,8 @@
-# Proporção (Porcentagem) dos por idade.
 import pandas as pd
-
 
 #fonte = 'https://github.com/alura-cursos/imersao-dados-2-2020/blob/master/MICRODADOS_ENEM_2019_SAMPLE_43278.csv?raw=true'
 fonte = './MICRODADOS_ENEM_2019_SAMPLE_43278.csv'
 
 dados = pd.read_csv(fonte)
-print(dados.columns.values)
+
+print(dados.head())
